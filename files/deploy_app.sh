098c2b6b014d7d892e3979a37371678820d2fab9
melonger <19414334+melonger@users.noreply.github.com>
2021-05-13 14:01:10 -0400

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Replace this text with your own.
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod ligula ante, nec blandit erat dapibus vitae. Curabitur venenatis orci ac augue viverra, at mattis orci hendrerit
   <!-- END -->
 
   </div>
